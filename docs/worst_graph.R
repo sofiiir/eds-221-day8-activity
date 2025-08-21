5782e4186ccf07adc1f7c4f764e52b296a6d73d6
sofiiir <sofiiirodas@gmail.com>
2025-08-20 18:31:32 -0700

    sf emissions

--- docs/worst_graph.R
+++ docs/worst_graph.R
@@ -14,6 +14,7 @@ cc <- scales::seq_gradient_pal("#FB00FF", "green")(seq(0, 1, length.out = 72))
 
 img<- png::readPNG("/Users/sofiarodas/Documents/MEDS/Summer/EDS-221/eds-221-day8-activity/figs/r-studio-logo.png")
 
+# image path saved
 img_path <- here::here("figs", "r-studio-logo.png")
 # Check if image is found
 if (!file.exists(img_path)) stop("Image not found at:", img_path)
@@ -26,25 +27,13 @@ ugly <- ggplot(data = space_launch, aes(x = launch_year, y = variant)) +
   theme_dark() +
   labs(title = "R", x = "variant", y = "launch.      year")+
   font("axis.title", size = 40, color = "red", face = "bold.italic")
- 
-  #aes(image(img))
-  #background_image(img)
-  #annotation_raster(img, xmin = 0, xmax = 0, ymin=0, ymax=0, interpolate = FALSE)
- # geom_tile_pattern(fill = 'white', launch_year = 'magenta', pattern = 'image', pattern_filename = 'r-studio-logo.png')
 
+# adds background to graph
 ggbackground(ugly, img_path)
 
 
 
-# read in sf emissions data
 
-sf_emissions <- read_csv(here("data", "San_Francisco_Communitywide_Greenhouse_Gas_Inventory.csv"))
-
-sf_emissions <- clean_names(sf_emissions)
-
-gge_sg <- sf_emissions |> 
-  group_by(sector_general) |> 
-  summarise(emissions_mt_co2e)
 
 
 
